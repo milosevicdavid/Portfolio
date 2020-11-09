ecdc3a8493c2f57a11afa37ad22ce3541ecde70c
David <milosevicdavid@gmail.com>
2020-11-09 23:59:21 +0100

    Landing page improved

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,72 @@
 <template>
+  <div class="icon-container">
+    <div class="icon-row">
+      <span class="icon">
+        <g-image src="~/assets/html.png" width="80"/>
+      </span> 
+      <span class="icon">
+        <g-image src="~/assets/css.svg" width="80"/>
+      </span> 
+      <span class="icon">
+        <g-image src="~/assets/graphql.png" width="80"/>
+      </span> 
+      <span class="icon">
+        <g-image src="~/assets/vue.png" width="80"/>
+      </span> 
+      <span class="icon">
+        <g-image src="~/assets/js.svg" width="80"/>
+      </span>
+      <span class="icon">
+        <g-image src="~/assets/sass.png" width="80"/>
+      </span>
+    </div>
+   <div class="icon-row">
+     <span class="icon">
+       <g-image src="~/assets/gridsome.svg" width="80"/>
+     </span>
+     <span class="icon">
+       <g-image src="~/assets/nuxt.png" width="80"/>
+     </span>
+     <span class="icon">
+       <g-image src="~/assets/vim.png" width="80"/>
+     </span>
+     <span class="icon">
+       <g-image src="~/assets/terminal.png" width="80"/>
+     </span>
+     <span class="icon">
+       <g-image src="~/assets/gatsby.png" width="80"/>
+     </span>
+     <span class="icon">
+       <g-image src="~/assets/linux.png" width="80"/>
+     </span>
+   </div>
+  </div>
 </template>
 
 <script>
 </script>
 
-<style>
+
+
+<style lang="scss">
+
+.icon-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  }
+
+.icon-row {
+  margin-top: 2rem;
+  text-align: center;
+ }
+
+.icon {
+  padding: 1.5rem;
+  line-height: 6rem;
+}
+
+
+
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,138 +1,176 @@
 <template>
- <Landing>
- <div class="index_wrapper">
-   <div class="index_left">
-       <g-link to="/about/">
-         <div class="center">
-           <div class="title">
-           <p>Portfolio</p>
-           </div>
-           <div class="description-text">
-      <p>
-        Welcome to my portfolio! My name is David Milosevic and I am a web developer
-        from Belgrade.
-      </p>
-           </div>
-         </div>
-       </g-link>
-   </div>
+   <Landing>
+      <div class="index_wrapper">
+         <div class="index_left">
+            <g-link to="/about/">
+               <div class="center">
+                  <p>
+                     <g-image class="svg" src="~/assets/cranium2.webp" width="260" />
+                  </p>
+                  <div class="title">
+                     <h1>Portfolio</h1>
+                  </div>
+                  <div class="description-text">
+                     <p>
+                        &nbsp; Welcome to my portfolio page! My name is David Milosevic and
+                        I am a web developer from Belgrade. Starting from here, you can get info about me, my projects and web development technologies that interests me, skills that I possess, contact info..
 
-   <div class="index-right">
-     <g-link to="/blog/">
-       <div class="center">
-         <p>Blog</p>
-       </div>
-     </g-link>
-   </div>
- </div>
- </Landing>
+                     </p>
+                  </div>
+               </div>
+            </g-link>
+         </div>
+
+         <div class="index-right">
+            <g-link to="/blog/">
+               <div class="center">
+                        <p>
+                           <g-image class="svg" src="~/assets/notebook.png" width="260"/>
+                        </p>
+                  <h1>Blog</h1>
+                  <div class="description-text">
+                     <p>
+                        &nbsp;  Discover the latest news in tech and web development in
+                        this articles section! The most knowledge about all kinds of technologies I am involved in.
+                     </p>
+                  </div>
+               </div>
+            </g-link>
+         </div>
+      </div>
+   </Landing>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: "Welcome!",
-  },
-  layout: 'Landing'
+   metaInfo: {
+      title: "Welcome!",
+   },
+   layout: "Landing",
 };
 </script>
 
 <style lang="scss">
-
-
 .index_wrapper {
-  font-family: "Orbitron", sans-serif;
-  display: flex;
+   font-family: "Orbitron", sans-serif;
+   display: flex;
 
-  @media (max-width: 576px) {
-    flex-direction: column;
-  }
+   @media (max-width: 576px) {
+      flex-direction: column;
+   }
 }
 .index_left {
-  width: 50%;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  border-right: 2px solid #2e8b57;
+   width: 50%;
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-around;
+   align-items: center;
+   border-right: 2px solid #2e8b57;
 
+   p {
+      margin: 2rem auto;
+      line-height: 1.5rem;
+      word-spacing: 0.4rem;
+   }
 
-  @media (max-width: 576px) {
-    width: 100%;
-    border-right: none;
-  }
+   @media (max-width: 576px) {
+      width: 100%;
+      border-right: none;
+   }
 }
- 
+
 .title {
-  p {
-    animation-name: moveInLeft;
-    animation-duration: 2s;
-    animation-timing-function: ease-out;
-  }
-  font-size: 3rem;
+   h1 {
+      animation-name: moveInLeft;
+      animation-duration: 2s;
+      animation-timing-function: ease-out;
+   }
+   font-size: 3rem;
 }
 
 .description-text {
 }
 
 .index-right {
-  font-size: 3rem;
-  height: 100vh;
-  display: flex;
-  border-left: 2px solid #2e8b57;
-  justify-content: center;
-  align-items: center;
-  width: 50%;
+   height: 100vh;
+   display: flex;
+   border-left: 2px solid #2e8b57;
+   justify-content: center;
+   align-items: center;
+   width: 50%;
 
-  p {
-    animation-name: moveInRight;
-    animation-duration: 2s;
-    animation-timing-function: ease-out;
-  }
+   p {
+      margin: 2rem auto;
+      line-height: 1.5rem;
+      word-spacing: 0.4rem;
+   }
 
+   h1 {
+      animation-name: moveInRight;
+      animation-duration: 2s;
+      animation-timing-function: ease-out;
+   }
 
-  @media (max-width: 576px) {
-    border-top: 2px solid #2e8b57;
-    border-left: none;
-    width: 100%;
-  }
+   @media (max-width: 576px) {
+      border-top: 2px solid #2e8b57;
+      border-left: none;
+      width: 100%;
+   }
+}
+
+.svg {
+   color: #363636;
+   fill: #363636;
 }
 
 .center {
-  cursor: pointer;
-  display: flex;
-  width: 30vw;
-  height: 300px;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  
-  @media (max-width: 576px) {
-    width: 70vw;
-  }
+   cursor: pointer;
+   display: flex;
+   width: 30vw;
+   height: 300px;
+   justify-content: center;
+   align-items: center;
+   flex-direction: column;
 
+   @media (max-width: 576px) {
+      width: 70vw;
+   }
+}
+
+.fade-enter-active {
+   transition: opacity 6s;
+}
+
+.fade-enter {
+   opacity: 0;
 }
 
 @keyframes moveInLeft {
-  0% {
-    opacity: 0;
-    transform: translateY(-10rem); }
-  80% {
-    transform: translateY(1rem); }
-  100% {
-    opacity: 1;
-    transform: translate(0); } }
+   0% {
+      opacity: 0;
+      transform: translateY(-10rem);
+   }
+   80% {
+      transform: translateY(1rem);
+   }
+   100% {
+      opacity: 1;
+      transform: translate(0);
+   }
+}
 
-  @keyframes moveInRight {
-  0% {
-    opacity: 0;
-    transform: translateY(10rem); }
-  80% {
-    transform: translateY(-1rem); }
-  100% {
-    opacity: 1;
-    transform: translate(0); } }
-
-
+@keyframes moveInRight {
+   0% {
+      opacity: 0;
+      transform: translateY(10rem);
+   }
+   80% {
+      transform: translateY(-1rem);
+   }
+   100% {
+      opacity: 1;
+      transform: translate(0);
+   }
+}
 </style>
